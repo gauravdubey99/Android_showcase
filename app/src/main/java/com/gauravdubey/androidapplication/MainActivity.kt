2aa0d39937cf26471a4097e74d79085fde2d3596
Gaurav Dubey <gaurav.dubey99@gmail.com>
2025-12-30 16:34:19 +0530

    Adding improvements to Input Field

--- app/src/main/java/com/gauravdubey/androidapplication/MainActivity.kt
+++ app/src/main/java/com/gauravdubey/androidapplication/MainActivity.kt
@@ -4,26 +4,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.gauravdubey.androidapplication.ui.theme.MyAndroidApplicationTheme
+import com.gauravdubey.androidapplication.widgets.ui.action.MyActionButton
+import com.gauravdubey.androidapplication.widgets.ui.input.MyInputFiledNumber
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    var inputNumber = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyAndroidApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val snackbarHostState = remember { SnackbarHostState() }
+                val scope = rememberCoroutineScope()
+                Scaffold(modifier = Modifier.fillMaxSize(), snackbarHost = { SnackbarHost(hostState = snackbarHostState) }) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        MyInputFiledNumber(Modifier.fillMaxWidth()) {
+                            inputNumber = it.toInt()
+                        }
+                        MyActionButton("Submit", Modifier.fillMaxWidth()) {
+                            scope.launch {
+                                snackbarHostState.showSnackbar("Input number is $inputNumber")
+                            }
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/com/gauravdubey/androidapplication/widgets/ui/input/InputFiledNumber.kt
+++ app/src/main/java/com/gauravdubey/androidapplication/widgets/ui/input/InputFiledNumber.kt
@@ -1,6 +1,7 @@
 package com.gauravdubey.androidapplication.widgets.ui.input
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -8,6 +9,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -18,7 +20,7 @@ fun MyInputFiledNumber(modifier: Modifier = Modifier, onValueChange: (String) ->
     TextField(value = inputText.toString(), modifier = modifier.padding(5.dp), onValueChange = { value ->
         onValueChange(value)
         inputText = value.toInt()
-    })
+    }, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
 }
 
 @Preview(showBackground = true)
